21b5d52c93d4aad0d880c03883834a8aec0741d5
Minh Mai <minhmmai@gmail.com>
2020-04-06 15:38:05 +1000

    change app port and address

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ const server = http.createServer((req, res) => {
     }
 });
 
-server.listen(1337, '127.0.0.1', () => {
+server.listen(3000, 'localhost', () => {
     console.log('Listening for requests now');
 });
 
